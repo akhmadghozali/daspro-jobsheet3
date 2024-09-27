3d101b251b3d105647eb48f97b65166275907dc3
akhmadghozali <akhmadghozali4414@gmail.com>
2024-09-27 09:27:40 +0700

    simpan kode java ke github

--- Kafe04.java
+++ Kafe04.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
         nominalBayar = totalHarga - (diskon *totalHarga);
 
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
-        System.out.println("Item pembelian " + jmlKopi + "kopi, " + jmlTeh + "teh, " + jmlRoti + "roti");
+        System.out.println("Item pembelian " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
         System.out.println("Nominal bayar Rp " +nominalBayar);
 
     }
